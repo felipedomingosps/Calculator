9b2408504ebab4cc7a4ad4ca65f9cda055f81939
Felipe Domingos <Felipedomingosps@gmail.com>
2022-08-31 19:19:19 -0300

    Add operate()

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ function divide(numberA, numberB) {
 function multiply(numberA, numberB) {
     return numberA * numberB
 }
+
+function operate(operator, numA, numB) {
+    if (operator === 'add') {
+        return add(numA, numB)
+    } else if (operator === 'subtract') {
+        return subtract(numA, numB)
+    } else if (operator === 'divide') {
+        return divide(numA, numB)
+    } else if (operator === 'multiply') {
+        return multiply(numA, numB)
+    }
+
+    return "error"
+}
