d2229f5e75fad02b9793950e0133545b348af501
Felipe Domingos <Felipedomingosps@gmail.com>
2022-08-31 19:04:49 -0300

    Add functions of basic math operations

---
+++ main.js
@@ -0,0 +1,15 @@
+function add(numberA, numberB) {
+    return numberA + numberB
+}
+
+function subtract(numberA, numberB) {
+    return numberA - numberB
+}
+
+function divide(numberA, numberB) {
+    return numberA / numberB
+}
+
+function multiply(numberA, numberB) {
+    return numberA * numberB
+}
