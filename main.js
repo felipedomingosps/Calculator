a81ce8d4cd7459ed11b009df94c042288b882cb3
Felipe Domingos <Felipedomingosps@gmail.com>
2022-09-11 18:30:54 -0300

    Add more classes to .calculator children

--- main.js
+++ main.js
@@ -37,3 +37,37 @@ function changeNumbers(number) {
     const numbersDisplay = document.querySelector('#numbers');
     numbersDisplay.innerHTML = number;
 }
+
+/*
+1
+- variável {numeroA} é criada
+- variável {operadorClicado} é criada
+- escreve um número
+- número aparece no display de forma dinâmica
+- sabemos o fim do número ao apertar no botão 'operador'
+- operador apertado é guardado dentro de {operadorClicado}
+- número é guardado dentro de {númeroA}
+*/
+
+function dummy() {
+    let numberA = "";
+    let operatorIsClicked = false;
+    const buttons = createButtons();
+
+    buttons.forEach(button => {
+        button.addEventListener('click', buttonClicked => {
+            if (['+', '-', '/', 'x','='].includes(buttonClicked.target.id)) {
+                
+            } else {
+                numberA += buttonClicked.target.id;
+                console.log(numberA)
+            } 
+        })
+
+    })
+
+    if (operatorIsClicked == true) {
+        return console.log('Number A - End')
+    }
+    
+}
