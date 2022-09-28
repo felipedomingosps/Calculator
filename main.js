e3667f044bd4f361d21ebcb2d50ac9ce103e3806
Felipe Domingos <Felipedomingosps@gmail.com>
2022-09-27 22:28:43 -0300

    Add calculator object and change functions

--- main.js
+++ main.js
@@ -1,44 +1,36 @@
-function main() {
-    let firstNumber;
-    let operatorClicked;
-    createButtons();
-
-
-    
+/* FUNCTIONS */ 
+function add(numberA, numberB) {
+    return Number(numberA) + Number(numberB)
 }
 
-function add(number, numberB) {
-    return number + numberB
+function subtract(numberA, numberB) {
+    return Number(numberA) + Number(numberB)
 }
 
-function subtract(number, numberB) {
-    return number - numberB
+function divide(numberA, numberB) {
+    return Number(numberA) + Number(numberB)
 }
 
-function divide(number, numberB) {
-    return number / numberB
-}
-
-function multiply(number, numberB) {
-    return number * numberB
+function multiply(numberA, numberB) {
+    return Number(numberA) + Number(numberB)
 }
 
 function operate(operator, numA, numB) {
-    if (operator === 'add') {
+    if (operator === '+') {
         return add(numA, numB)
-    } else if (operator === 'subtract') {
+    } else if (operator === '-') {
         return subtract(numA, numB)
-    } else if (operator === 'divide') {
+    } else if (operator === '/') {
         return divide(numA, numB)
-    } else if (operator === 'multiply') {
+    } else if (operator === '*') {
         return multiply(numA, numB)
     }
 
     return "error"
 }
 
-function createButtons() {
-    return document.querySelectorAll('.calculator__element').forEach(button => {
+function createNumberButtons() {
+    return document.querySelectorAll('.calculator__element--number').forEach(button => {
         button.addEventListener('click', e => {
                 if (e.target.dataset.number != null) {
                     changeDisplay(e.target.dataset.number)
@@ -48,10 +40,46 @@ function createButtons() {
     })
 }
 
+function createOperatorButtons() {
+    return document.querySelectorAll('.calculator__element--operator').forEach(button => {
+        button.addEventListener('click', e => {
+                if (e.target.dataset.operator != '=') {
+                    if (calculator.firstNumberIsDone === false) {
+                        calculator.firstNumber = document.querySelector('#numbers').innerHTML
+                        calculator.firstNumberIsDone = true;
+                        calculator.operatorPressed = e.target.dataset.operator
+                        clearDisplay()
+                    } else {
+                        calculator.secondNumber = document.querySelector('#numbers').innerHTML
+                        calculator.secondNumberIsDone = true;
+                        clearDisplay()
+                    }
+                } else {
+                    changeDisplay(operate(calculator.operatorPressed, calculator.firstNumber, calculator.secondNumber))
+                }
+                
+        })
+    })
+}
+
+
+
 function changeDisplay(number) {
-    return document.querySelector('#numbers').innerHTML += number
+    document.querySelector('#numbers').innerHTML += number
 }
 
 function clearDisplay() {
     return document.querySelector('#numbers').innerHTML = '';
 }
+
+/* APP */
+let calculator = {
+    firstNumber: 0,
+    secondNumber: 0,
+    firstNumberIsDone: false,
+    secondNumberIsDone: false,
+    operatorPressed: null
+};
+
+createNumberButtons()
+createOperatorButtons()
